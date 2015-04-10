38d8b9370889a68946b8622a80b24bee4a513f0a
Jon Edwards <jkedwards@me.com>
2015-04-10 15:22:51 -0700

    working AJAX pull from quantum number generator and display

--- client/index.js
+++ client/index.js
@@ -1,2 +1,24 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+
+function init(){
+  $('#getRandom').click(getRandom);
+}
+
+function getRandom(){
+  var number = $('#number').val();
+  $.getJSON('https://qrng.anu.edu.au/API/jsonI.php?length=' + number + '&type=uint16', function(response){
+    response.data.forEach(function(number) {
+    });
+    $('#sum').val(sum(response.data));
+  });
+}
+
+function sum(numbers){
+  var total = 0;
+  numbers.forEach(function(number) {
+    total += number;
+  });
+  return total;
+}
